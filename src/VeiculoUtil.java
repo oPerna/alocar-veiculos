21b5eae53372b7866c46cad372762277551b31fb
Rafael de Jesus <99143563+oPerna@users.noreply.github.com>
2023-06-06 13:19:37 +0000

    Commit

--- src/VeiculoUtil.java
+++ src/VeiculoUtil.java
@@ -8,11 +8,43 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class VeiculoUtil {
+    public static List<Veiculo> LocacaoDeVeiculos = new ArrayList<>();
     
     public static void main(String[] args) throws Exception {
        Scanner entrada = new Scanner(System.in);
+       char optionSelected;
+       do {
+           System.out.println("A. Cadastro de Veículos.");
+           System.out.println("B. Consulta de Veículos");
+           System.out.println("C. ");
+           System.out.println("D. ");
+           System.out.println("E. ");
+           optionSelected = entrada.next().charAt(0);
 
+           switch (optionSelected) {
+               case 'A':
+               case 'a':
+                   
+                   break;
+               
+               case 'B':
+               case 'b':
+
+                   break;
+
+               case 'C':
+               case 'c':
        
-        
+                   break;
+
+               case 'D':
+               case 'd':
+       
+                   break;
+
+               default:
+                   break;
+           }
+       } while (optionSelected != 'e');
     }
 }
