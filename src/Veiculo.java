f50c1a6bec09e302163b2d9a0b1363345a69ed3d
Rafael de Jesus <99143563+oPerna@users.noreply.github.com>
2023-06-06 13:03:48 +0000

    espaço

--- src/Veiculo.java
+++ src/Veiculo.java
@@ -15,24 +15,28 @@ public class Veiculo implements Serializable {
     public void setPlaca(String placa) {
         this.placa = placa;
     }
+
     public String getModelo() {
         return modelo;
     }
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
+
     public int getAno() {
         return ano;
     }
     public void setAno(int ano) {
         this.ano = ano;
     }
+
     public String getCor() {
         return cor;
     }
     public void setCor(String cor) {
         this.cor = cor;
     }
+    
     public int getQuilometragem() {
         return quilometragem;
     }
