9090a1b1acbfa41d2d0e1ea461730e95364c8ab3
Jaumaor <110685799+Jaumaor@users.noreply.github.com>
2023-06-06 12:58:37 +0000

    VEICULO

---
+++ ALOCAR-VEICULOS/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("ola galerinha do feicibuk");
+        System.out.println("😍❤❤🤣😍❤❤");
+    }
+}
--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class App {
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Scanner entrada = new Scanner(System.in);
+
+       
+        
     }
 }
