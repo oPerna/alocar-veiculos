eba7561581d8904c4ead99e8938466fa3d525690
Rafael de Jesus <99143563+oPerna@users.noreply.github.com>
2023-06-06 13:04:35 +0000

    apagado

--- ALOCAR-VEICULOS/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("ola galerinha do feicibuk");
-        System.out.println("😍❤❤🤣😍❤❤");
-    }
-}
